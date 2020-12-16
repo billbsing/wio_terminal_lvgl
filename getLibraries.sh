4056333b97d64c7a8fa9ba98c1d0c9ba0be04847
bill <billbsing@gmail.com>
2020-12-16 14:42:52 +0800

    load libs for testing

--- getLibraries.sh
+++ getLibraries.sh
@@ -4,6 +4,6 @@ LIBRARY_FOLDER=libraries
 rm -rf $LIBRARY_FOLDER
 mkdir $LIBRARY_FOLDER
 cd $LIBRARY_FOLDER
-# git clone https://github.com/Seeed-Studio/Seeed_Arduino_LvGL.git
+git clone https://github.com/Seeed-Studio/Seeed_Arduino_LCD.git
 git clone https://github.com/lvgl/lvgl.git
 cp ../lvgl_include/lv_conf.h .
